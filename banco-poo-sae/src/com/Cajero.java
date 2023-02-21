2370d94fae71c7f93bb8856e7fc550815269e8d6
faanuca <stephany.andren@gmail.com>
2023-02-21 14:05:27 -0600

    ejercicio banco sae

--- banco-poo-sae/src/com/Cajero.java
+++ banco-poo-sae/src/com/Cajero.java
@@ -4,14 +4,12 @@ import java.util.Date; //UTIL
 
 //EL IMPLEMENTS SIRVE PARA-
 
-public class Cajero implements IOperacionesBancarias{
-	
+public class Cajero implements IOperacionesBancarias {
+
 	String ubicacion;
-	int folio =0;
-	
-	
-	
-	public Cajero () {	
+	int folio = 0;
+
+	public Cajero() {
 	}
 
 	public Cajero(String ubicacion) {
@@ -33,71 +31,65 @@ public class Cajero implements IOperacionesBancarias{
 	}
 
 	@Override
-	public Ticket depositar (Cuenta cuenta, double monto) {
-	
-		
-Ticket comprobante = null;
-		
-		if(monto>cuenta.getMax() ) {
+	public Ticket depositar(Cuenta cuenta, double monto) {
+
+		Ticket comprobante = null;
+
+		if (monto > cuenta.getMax()) {
 			System.out.println("Cantidad excede el maximo permitido");
 		} else {
-			cuenta.setSaldoDisponible(cuenta.getSaldoDisponible()+ monto);
-			return new Ticket (this.ubicacion,
-					new Date(), monto, cuenta.getNumCuenta(), "Deposito", Integer.toString(this.folio));
+			cuenta.setSaldoDisponible(cuenta.getSaldoDisponible() + monto);
+			return new Ticket(this.ubicacion, new Date(), monto, cuenta.getNumCuenta(), "Deposito",
+					Integer.toString(this.folio));
 		}
 		return comprobante;
-		
-		
+
 	}
 
 	@Override
 	public Ticket retirar(Cuenta cuenta, double monto) {
-		//saldo suficiente
-		//que no deje por debajo del minimo de la cuenta
-	
-Ticket comprobante = null;
-		
-		if(cuenta.getSaldoDisponible()<monto ) {
+		// saldo suficiente
+		// que no deje por debajo del minimo de la cuenta
+
+		Ticket comprobante = null;
+
+		if (cuenta.getSaldoDisponible() < monto) {
 			System.out.println("Saldo insuficiente");
-		} else if ((cuenta.getSaldoDisponible()-monto) <cuenta.getMin()); {
+		} else if ((cuenta.getSaldoDisponible() - monto) < cuenta.getMin()) {
 			System.out.println("Retiro excederia el minimo permitido");
 		} else {
-			cuenta.setSaldoDisponible(cuenta.getSaldoDisponible()-monto);
-		
-			
-			
-			return new Ticket (this.ubicacion, new Date(), monto, cuenta.getNumCuenta(), "Retiro", Integer.toString(this.folio++));
-			
+			cuenta.setSaldoDisponible(cuenta.getSaldoDisponible() - monto);
+
+			return new Ticket(this.ubicacion, new Date(), monto, cuenta.getNumCuenta(), "Retiro",
+					Integer.toString(this.folio++));
+
 		}
 		return comprobante;
 	}
 
 	@Override
-	public Ticket transferencia (Cuenta origen, Cuenta destino, double monto) {
+	public Ticket transferencia(Cuenta origen, Cuenta destino, double monto) {
 		// TODO Auto-generated method stub
 		Ticket comprobante = null;
-		
-		if(origen.getSaldoDisponible()<monto ) {
+
+		if (origen.getSaldoDisponible() < monto) {
 			System.out.println("Saldo insuficiente");
-		}else if ((origen.getSaldoDisponible()-monto) < origen.getMin()) {
+		} else if ((origen.getSaldoDisponible() - monto) < origen.getMin()) {
 			System.out.println("Retiro excederia el minimo permitido");
-		}else if(monto>destino.getMax()) {
+		} else if (monto > destino.getMax()) {
 			System.out.println("Cantidad excede el maximo permitido");
-		}else if((destino.getSaldoDisponible()+monto) > destino.getMax()) {
+		} else if ((destino.getSaldoDisponible() + monto) > destino.getMax()) {
 			System.out.println("Deposito excederia el maximo permitido");
-		}else {
-			origen.setSaldoDisponible(origen.getSaldoDisponible()- monto);
-			destino.setSaldoDisponible(destino.getSaldoDisponible()+monto);
-			return new Ticket (this.ubicacion, new Date(),
-					monto, destino.getNumCuenta(), "Transferencia",
-					Integer.toBinaryString(this.folio++)
-			
+		} else {
+			origen.setSaldoDisponible(origen.getSaldoDisponible() - monto);
+			destino.setSaldoDisponible(destino.getSaldoDisponible() + monto);
+			return new Ticket(this.ubicacion, new Date(), monto, destino.getNumCuenta(), "Transferencia",
+					Integer.toString(this.folio++));
+
 		}
-		}
-		return null;
+
+		return comprobante;
 		
-		
-		
-		}
 	}
 
+}
--- banco-poo-sae/src/com/Principal.java
+++ banco-poo-sae/src/com/Principal.java
@@ -9,7 +9,7 @@ public class Principal {
 		
 		Cajero atm = new Cajero ("Avenida Juarez, La Paz");
 		
-		Cuenta c1 = new Cuenta (50.00 "Debito", "C0001"," RTVGETR", 10.00, 2000.00);
+		Cuenta c1 = new Cuenta (50.00, "Debito", "C0001"," RTVGETR", 10.00, 2000.00);
 		atm.retirar(c1, 470.00);
 		
 		System.out.println(c1);
