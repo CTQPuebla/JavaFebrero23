ce4d70473fac21692e8e0cde92c9c2173c86e8a8
miguel09js <miguel.jusa29@outlook.com>
2023-02-21 13:50:00 -0600

    ejercicio cajero majs

---
+++ banca-poo-majs/src/com/Cliente.java
@@ -0,0 +1,63 @@
+package com;
+
+public class Cliente {
+
+	String nombre;
+	int idCliente;
+	String direccion;
+	String rfc;
+	
+	public Cliente(){}
+
+	public Cliente(String nombre, int idCliente, String direccion, String rfc) {
+		//super();
+		this.nombre = nombre;
+		this.idCliente = idCliente;
+		this.direccion = direccion;
+		this.rfc = rfc;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getRfc() {
+		return rfc;
+	}
+
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", idCliente=" + idCliente + ", direccion=" + direccion + ", rfc=" + rfc
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+}
